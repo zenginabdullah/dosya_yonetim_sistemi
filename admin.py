bc2c55fd995f0d5aa9e591249b9c933dc01337cd
Elif Yetim <zenginn.abdullah@gmail.com>
2024-12-27 12:58:06 +0300

    Admin fonksiyonları eklendi.
    Kullanıcı silme, hashli şifreyi görüntüleme, logları exportlama fonksiyonları ve arayüzleri eklendi.

--- admin.py
+++ admin.py
@@ -68,3 +68,51 @@ class Admin:
         else:
             messagebox.showerror("Hata", f"Kullanıcı ID {user_id} bulunamadı.")
             return
+        
+    def delete_user(user_id):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM users WHERE id = ?", (user_id,))
+        user = cursor.fetchone()
+
+        if user:
+            cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
+            conn.commit()
+            conn.close()
+            messagebox.showinfo("Başarılı", "Kullanıcı başarıyla silindi.")
+        else:
+            conn.close()
+            messagebox.showerror("Hata", "Kullanıcı bulunamadı.")
+    
+    def view_encrypted_password(user_id):
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT password FROM users WHERE id = ?", (user_id,))
+        result = cursor.fetchone()
+        conn.close()
+
+        if result:
+            messagebox.showinfo("Şifre", f"Kullanıcının Şifrelenmiş Parolası: {result[0]}")
+        else:
+            messagebox.showerror("Hata", "Kullanıcı bulunamadı.")
+    
+    def view_logs():
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT timestamp, user_id, action FROM logs ORDER BY timestamp DESC")
+        logs = cursor.fetchall()
+        conn.close()
+
+        if logs:
+            # Logları metin formatına dönüştür
+            log_details = "\n".join([f"Tarih: {log[0]}, Kullanıcı ID: {log[1]}, İşlem: {log[2]}" for log in logs])
+            
+            # Logları bir dosyaya yaz
+            with open("logs.txt", "w", encoding="utf-8") as log_file:
+                log_file.write("Sistem Logları\n")
+                log_file.write("=" * 50 + "\n")
+                log_file.write(log_details)
+            
+            messagebox.showinfo("Log Dosyaları", f"Tüm loglar 'logs.txt' dosyasına başarıyla kaydedildi.")
+        else:
+            messagebox.showinfo("Log Dosyaları", "Hiçbir işlem kaydı bulunamadı.")
